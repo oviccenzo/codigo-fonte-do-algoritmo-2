036c423e0a1261d64bfdd7dddbd9272cfae01a36
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-04-02 18:21:39 -0300

    Add files via upload

---
+++ LinguagemC-Alura/exemplo.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include "nao_acertou.cpp"
+#include "letra_existe.cpp"
+
+using namespace std;
+
+string palavra_secreta;
+map<char, bool> chutou;
+vector<char> chutes_errado;
+
+void imprime_erros(){
+    cout << "Chutes errados: ";
+    for(char letra: chutes_errado){
+        cout << letra << " ";
+    }
+    cout << endl;
+}
+
+void imprime_palavra(){
+    for(char letra : palavra_secreta){
+        if(chutou[letra]){
+            cout << letra << " ";
+        }
+        else{
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+void chuta(){
+    cout << "Seu chute: ";
+    char chute;
+    cin >> chute;
+
+    chutou[chute] = true;
+
+    if(letra_existe(chute)){
+        cout << "Voce acertou! Seu chute está na palavra." << endl;
+    }
+    else{
+        cout << "Voce acertou! Seu chute não está palavra." << endl;
+        chutes_errado.push_back(chute);
+    }
+    cout << endl;
+}
+
+bool letra_existe(char chute) {
+    return false;
+}
+
+vector<string> le_arquivo(){
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+
+    if(arquivo.is_open()){
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
+
+
+        vector<string> palavras_do_arquivo;
+
+        for(int i = 0; i < quantidade_palavras; i++){
+            string palavras_lida;
+            arquivo >> palavras_lida;
+            palavras_do_arquivo.push_back(palavras_lida);
+        }
+        arquivo.close();
+        return palavras_do_arquivo;
+    }
+
+    else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void sorteia_palavra(){
+    vector<string> palavras = le_arquivo();
+
+    srand(time(nullptr));
+    int indice_sorteado = rand() % palavras.size();
+
+    palavra_secreta = palavras[indice_sorteado];
+}
+
+void salva_arquivo(const vector<string>& nova_lista){
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+    if(arquivo.is_open()){
+        arquivo << nova_lista.size() << endl;
+
+        for(string palavra : nova_lista){
+            arquivo << palavra << endl;
+        }
+        arquivo.close();
+    }
+    else{
+        cout << "Não foi posivel acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void adiciona_palavra(){
+    cout << "Digite a nova palavra, usando letras maiusculas." << endl;
+    string nova_palavra;
+    cin >> nova_palavra;
+
+    vector<string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+
+    salva_arquivo(lista_palavras);
+}
+
+int main() {
+    imprime_cabecalho();
+
+    le_arquivo();
+    sorteia_palavra();
+
+    while(nao_acertou() && nao_enforcou()){
+        imprime_erros();
+
+        imprime_palavra();
+
+        chuta();
+    }
+
+    cout << " Fim de jogo!" << endl;
+    cout << " A palavra secreta era: " << palavra_secreta << endl;
+    if(nao_acertou()){
+        cout << " Você perdeu! Tente novamente! " << endl;
+    }
+    else{
+        cout << " Parabéns! Você acertou a palavra secreta! " << endl;
+
+        cout << "Voce deseja adicionar uma palavra ao banco? (S/N)";
+        char resposta;
+        cin >> resposta;
+        if(resposta == 'S'){
+            adiciona_palavra();
+        }
+    }
+
+    cin.get();
+}
---
+++ LinguagemC-Alura/imprime_cabeçalho.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void imprime_cabeçalho(){
+    std::cout << "********************" << std::endl;
+    std::cout << "***Jogo da Forca ***" << std::endl;
+    std::cout << std::endl;
+}
